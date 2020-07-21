95b89ccc2e3dc1c8ef7ebec21d61eb476d74d30c
Rajesh <44103581+kalven22@users.noreply.github.com>
2020-07-20 21:25:45 -0400

    Update bigboss2.php

--- bigboss2.php
+++ bigboss2.php
@@ -14,8 +14,9 @@
 	define( 'PATH_TCLASS', dirname(__FILE__) . "/" );
 	$path = dirname(dirname(__FILE__)) . "/app.ajax-poll/";
 	if ( !file_exists( $path ) ) {
-		echo "<h3>Big Boss 2 season has ended!</h3>";
-    		echo "<h3>More information about the new season release will be updated soon.</h3>";
+		echo "<h3><style text-align:center>Big Boss 2 season has ended!</style></h3>";
+    		echo "<h3>More information about the new season's release will be updated here soon.</h3>";
+		echo "<h3>Please check back here later.</h3>";
     		echo "<h3>Thank You for your patience!</h3>";
 		exit;
 	}
